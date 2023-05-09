9a9fd90678f73cc11b5ea8bd1e2a736bad912372
keple <keplerde@gmail.com>
2023-05-09 04:40:21 +0200

    React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
@@ -7,12 +8,20 @@ function Header() {
       <Navbar bg="dark" variant="dark " expand="lg" collapseOnSelect>
 
         <Container>
-          <Navbar.Brand href="#home">ATIShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand href="#home">ATIShop</Navbar.Brand>
+          </LinkContainer>
+          
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-              <Nav.Link href="#home"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-              <Nav.Link href="#link"><i className="fas fa-user"></i>Login</Nav.Link>
+              <LinkContainer to='/cart'>
+              <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='/login'>
+              <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+              </LinkContainer>
           </Nav>   
           </Navbar.Collapse>
         </Container>
