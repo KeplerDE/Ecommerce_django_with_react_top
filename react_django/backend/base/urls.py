fde16c9564a4df854cd3e74cf755f3d6e20d7bb7
keple <keplerde@gmail.com>
2023-05-31 13:42:20 +0200

    registerUser

--- react_django/backend/base/urls.py
+++ react_django/backend/base/urls.py
@@ -5,7 +5,8 @@ from .views import MyTokenObtainPairView
 urlpatterns = [
     path('users/login/', MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
-    path('', views.getRoutes, name="routes"),
+    # path('', views.getRoutes, name="routes"),
+    path('users/register/', views.registerUser, name='register'),
     path('users/profile/', views.getUserProfile, name="users-profile"),
     path('users/', views.getUsers, name="users"),
 
